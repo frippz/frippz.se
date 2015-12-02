3e35ed1c3cc07434a1beebf7f92820eb2131ca69
Fredrik Frodlund <fredrik@frodlund.se>
2015-12-02 14:41:53 +0100

    Remove gui from version control

--- gui/js/main.js
+++
@@ -1,2 +0,0 @@
-"querySelector"in document&&"classList"in document.createElement("a")&&"addEventListener"in window&&document.querySelector("html").classList.add("js");
-//# sourceMappingURL=main.js.map
