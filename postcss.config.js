7621b583dfa6d5f38170c6bb742832f4ab6e4c73
Fredrik Frodlund <fredrik@frodlund.se>
2019-01-18 22:48:28 +0100

    Restore postcss-custom-properties

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,11 @@
 /* eslint global-require:0 */
 
 module.exports = {
-  map: { inline: true }
+  map: { inline: true },
+  plugins: [
+    require('postcss-custom-properties')({
+      preserve: true,
+      importFrom: 'src/css/00_variables.css'
+    }),
+  ]
 };
--- postcss.config.live.js
+++ postcss.config.live.js
@@ -1,5 +1,11 @@
 /* eslint global-require:0 */
 
 module.exports = {
-  map: { inline: false }
+  map: { inline: false },
+  plugins: [
+    require('postcss-custom-properties')({
+      preserve: true,
+      importFrom: 'src/css/00_variables.css'
+    }),
+  ]
 };
