e2b7478eec6c11e5c29b31760e86ce1c54ebbed0
Fredrik Frodlund <fredrik@frodlund.se>
2019-02-25 17:27:40 +0100

    Remove requireJS & PostCSS
    Let Jekyll serve files directly and add support for hashing in production

---
+++ js/main.js
@@ -0,0 +1,35 @@
+/**
+ * main.js
+ */
+
+'use strict';
+
+// Only run in capable browsers via feature detection
+var didCutTheMustard = 'querySelector' in document
+  && 'classList' in document.createElement('a')
+  && 'addEventListener' in window;
+
+if (didCutTheMustard) {
+
+  // Add class "js" to html element
+  document.querySelector('html').classList.add('js');
+
+  /**
+   * Add line numbers to <pre>
+   */
+  (function () {
+    var pre = document.querySelectorAll('pre'),
+        pl = pre.length;
+
+    for (var i = 0; i < pl; i++) {
+      pre[i].insertAdjacentHTML('beforeend', '<span class="ln" aria-hidden="true"></span>');
+      var num = pre[i].innerHTML.split(/\n/).length;
+
+      // Start counter at 1 to circumvent Jekyll adding an extra line in <code>
+      for (var j = 1; j < num; j++) {
+        var lineNum = pre[i].querySelector('.ln');
+        lineNum.insertAdjacentHTML('beforeend', '<span></span>');
+      }
+    }
+  })();
+}
