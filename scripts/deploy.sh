45331cc9318f3d79dafd36cb862afb30e790dbdb
Fredrik Frodlund <fredrik@frodlund.se>
2019-09-06 08:35:39 +0200

    Use env vars

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@
 
 yarn install --production --frozen-lockfile --cache-folder /srv/jekyll/.yarn-cache
 yarn run deploy
-JEKYLL_ENV=production jekyll build --config "_config.yml,_config.prod.yml"
+jekyll build --config "_config.yml,_config.prod.yml"
