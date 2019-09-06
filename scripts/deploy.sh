230654f6e924ffa9822cd77cd7d5fcb14d5a5031
Fredrik Frodlund <fredrik@frodlund.se>
2019-09-06 08:55:16 +0200

    Update deploy scripts Fri Sep  6 08:55:16 CEST 2019

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir /.yarn
-yarn install --production --frozen-lockfile
+mkdir /srv/jekyll/.yarn
+yarn install --production --frozen-lockfile --prefix /srv/jekyll/.yarn
 yarn run deploy
 jekyll build --config "_config.yml,_config.prod.yml"
