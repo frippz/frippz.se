b56297c7d26ae32c07cc6ee16c751be1c1b12fb7
Fredrik Frodlund <fredrik@frodlund.se>
2019-09-06 08:47:06 +0200

    Change deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+mkdir /srv/jekyll/.yarn-cache
 yarn install --production --frozen-lockfile --cache-folder /srv/jekyll/.yarn-cache
 yarn run deploy
 jekyll build --config "_config.yml,_config.prod.yml"
