aa6b7203244b3d7ceea58c63e25df68f3ab46864
Fredrik Frodlund <fredrik@frodlund.se>
2019-09-06 08:33:54 +0200

    Move arguments

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-yarn install --production --frozen-lockfile
+yarn install --production --frozen-lockfile --cache-folder /srv/jekyll/.yarn-cache
 yarn run deploy
 JEKYLL_ENV=production jekyll build --config "_config.yml,_config.prod.yml"
