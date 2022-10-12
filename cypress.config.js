cf68e31c6be5424ce86f6b9db7324b4269b75da0
Fredrik Frodlund <fredrik@frodlund.se>
2022-10-12 14:35:18 +0200

    Disable video recording in Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  retries: 3,
+  video: false,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
