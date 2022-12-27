5af938476f5cafc9782b7758ee399dc0c6a33f3b
Fredrik Frodlund <fredrik@frodlund.se>
2022-12-27 18:41:47 +0100

    Run Prettier

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   retries: 3,
   video: false,
   e2e: {
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require('./cypress/plugins/index.js')(on, config);
     },
   },
-})
+});
--- js/main.js
+++ js/main.js
@@ -8,34 +8,40 @@
 
 /* global CSS */
 
-'use strict'
+'use strict';
 
 // Only run in capable browsers via feature detection
-const didCutTheMustard = 'querySelector' in document &&
+const didCutTheMustard =
+  'querySelector' in document &&
   'classList' in document.createElement('a') &&
   'addEventListener' in window &&
-  CSS.supports('display', 'flex')
+  CSS.supports('display', 'flex');
+
+/**
+ * Add line numbers to <pre>
+ */
+const lineNumbers = () => {
+  const pre = document.querySelectorAll('pre');
+  const pl = pre.length;
+
+  for (let i = 0; i < pl; i++) {
+    pre[i].insertAdjacentHTML(
+      'beforeend',
+      '<span class="ln" aria-hidden="true"></span>'
+    );
+    const num = pre[i].innerHTML.split(/\n/).length;
+
+    // Start counter at 1 to circumvent Jekyll adding an extra line in <code>
+    for (let j = 1; j < num; j++) {
+      const lineNum = pre[i].querySelector('.ln');
+      lineNum.insertAdjacentHTML('beforeend', '<span></span>');
+    }
+  }
+};
 
 if (didCutTheMustard) {
   // Add class "js" to html element
   document.querySelector('html').classList.add('js');
 
-  /**
-   * Add line numbers to <pre>
-   */
-  (function () {
-    const pre = document.querySelectorAll('pre')
-    const pl = pre.length
-
-    for (let i = 0; i < pl; i++) {
-      pre[i].insertAdjacentHTML('beforeend', '<span class="ln" aria-hidden="true"></span>')
-      const num = pre[i].innerHTML.split(/\n/).length
-
-      // Start counter at 1 to circumvent Jekyll adding an extra line in <code>
-      for (let j = 1; j < num; j++) {
-        const lineNum = pre[i].querySelector('.ln')
-        lineNum.insertAdjacentHTML('beforeend', '<span></span>')
-      }
-    }
-  })()
+  lineNumbers();
 }
